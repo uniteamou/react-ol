3f767694adb5937d1c7dc3bd153f41afe35b318a
Adilkhan <adilkhankenzhetaev@gmail.com>
2025-11-21 20:23:23 +0200

    docs: introduce astro build for docs website

---
+++ docs_website/astro.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/uniteamou/react-ol',
+        },
+      ],
+      sidebar: [
+        {
+          label: 'Guides',
+          autogenerate: { directory: 'guides' },
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+})
